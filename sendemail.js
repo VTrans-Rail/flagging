74e593468e122fcd4233c7a602c5f3dd32941ff0
Stephen <stephen@mapsmith.net>
2016-09-06 12:55:03 -0400

    temp comment out send email code and replace with console.log

--- sendemail.js
+++ sendemail.js
@@ -1,15 +1,16 @@
 function sendEmail (emailSubmission) {
     // parameters: service_id, template_id, template_parameters
-  try {
-    emailjs.send('sendgrid', 'vrs', emailSubmission)
-    .then(function (response) {
-      console.log('successful email')
-    }, function (err) {
-      console.error('failed - error = ', err)
-    })
-  } catch (e) {
-    console.error(e)
-  } finally {
-
-  }
+  console.log('email test sent')
+  // try {
+  //   emailjs.send('sendgrid', 'vrs', emailSubmission)
+  //   .then(function (response) {
+  //     console.log('successful email')
+  //   }, function (err) {
+  //     console.error('failed - error = ', err)
+  //   })
+  // } catch (e) {
+  //   console.error(e)
+  // } finally {
+  //   // TODO: show confirmation that the email was sent
+  // }
 }
