896355f7fc98be27639cee50a488ad6b8c9c7e16
Stephen <stephen@mapsmith.net>
2016-09-12 15:31:34 -0400

    form population setup for status

--- status.js
+++ status.js
@@ -56,9 +56,6 @@ require([
 
   var feature = '' // make the feature var in the global scope to allow access later
 
-  on(dom.byId('approve'), 'click', function () { submit('Approve') })
-  on(dom.byId('reject'), 'click', function () { submit('Reject') })
-
   // -------------------------------------------------------------------
   // ------------Setup Map & symbol -------------------------------
   // -------------------------------------------------------------
@@ -109,37 +106,25 @@ require([
     // setup the graphic of the one result feature
     feature = new Graphic(results.features[0].geometry, symbol, results.features[0].attributes)
 
-    if (feature.attributes.RPMDecision && formType === 'vtrans') { // check is the request has been previously approved
-      document.getElementById('alertTop').style.display = 'block'
-      document.getElementById('alertBot').style.display = 'block'
-      document.getElementById('agentName').value = feature.attributes.RPMApprovalBy
-      document.getElementById('comments').value = feature.attributes.RPMComment
+    var displayFields = ['AppDate']
+
+    var reqStatus = ['<h2><i class="glyphicon glyphicon-ok"></i></h2><p><strong>Submitted </strong><i><span id="AppDate"></span></i></p>']
+
+    dom.byId('req-status').innerHTML = reqStatus.join('')
+
+    if (feature.attributes.RPMDecision) { // if VTrans has approved
+      displayFields.push('RPMDecision', 'RPMDecisionDate')
+      reqStatus = ['<h2><i class="glyphicon glyphicon-ok"></i></h2><p><strong>Approved </strong><i><span id="RPMDecisionDate"></span></i></p>']
+      dom.byId('rpm-status').innerHTML = reqStatus.join('')
     }
 
-    if (feature.attributes.RRDecision && formType === 'vrs') { // check is the request has been previously approved
-      document.getElementById('alertTop').style.display = 'block'
-      document.getElementById('alertBot').style.display = 'block'
-      document.getElementById('agentName').value = feature.attributes.RRApprovedBy
-      document.getElementById('flaggerName').value = feature.attributes.RRFlagger
+    if (feature.attributes.RRDecision) {
+      displayFields.push('RRDecision', 'RRDecisionDate')
+      reqStatus = ['<h2><i class="glyphicon glyphicon-ok"></i></h2><p><strong>Approved </strong><i><span id="RRDecisionDate"></span></i></p>']
+      dom.byId('vrs-status').innerHTML = reqStatus.join('')
     }
-
-    var makeSpans = [] // create one <span> for each `outField`
-
-    var displayFields = [ // fields as field names
-      'AppDate',
-      'CompName', 'VTransProject', 'BillAddress', 'BillTown', 'BillState', 'BillZIP',
-      'CompType', 'AppName', 'AppPhone', 'AppEmail', 'WorkRR', 'WorkTown',
-      'WorkFromMP', 'WorkToMP', 'WorkDuration', 'WorkStartDate', 'WorkCompletionDate', 'WorkDescription',
-      'WorkEquipment', 'WorkCompletionDate', 'WorkAsset'
-    ]
 
     var displayArray = results.fields.filter(function (field) { return (displayFields.indexOf(field.name) >= 0) })
-
-    // TODO: Fix which fields are displayed (rather than ouftields = *)
-    for (var field in displayArray) {
-      makeSpans.push('<strong>' + displayArray[field].alias + ': </strong>' + '<span class="data" id="' + displayArray[field].name + '"></span><br>')
-    }
-    dom.byId('full-info').innerHTML = makeSpans.join('') // populate the dom with the makeSpans and join each element with a ''
 
     var resultCount = results.features.length // number of attributes returned
     for (var i = 0; i < resultCount; i++) { // loop through each attribute
@@ -151,7 +136,7 @@ require([
             var d = new Date(featureAttributes[attr])
             var n = document.querySelectorAll('#' + attr)
             for (var j = 0; j < n.length; j++) {
-              n[j].innerHTML = d.format('dddd, mmmm dS, yyyy')
+              n[j].innerHTML = d.format('mmmm dS, yyyy')
             }
           } else { // handle all other cases like this
             resultItems.push('<strong>' + attr + ': </strong>' + featureAttributes[attr] + '</br>')
