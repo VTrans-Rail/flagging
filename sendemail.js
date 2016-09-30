984385d13609420e95c93dbe4d20cf5bf12faca3
Stephen <stephen@mapsmith.net>
2016-09-30 14:35:54 -0400

    setup framework for sending multiple emails

--- sendemail.js
+++ sendemail.js
@@ -14,48 +14,98 @@ function sendEmail (emailFormParams) {
     return json
   })()
 
-  var params = {} // object to pass to sending function
-
-  if (emailFormParams.source === 'form' && emailFormParams.email_type === 'exception') {
-    params = {
-      to: emailFormParams.req_email,
-      from_name: 'VTrans Rail Division',
-      from_email: json.recipients.mark,
-      reply_to: json.recipients.mark,
-      cc: json.recipients.rpm,
-      bcc: json.recipients.gis,
-      subject: json.subject.user.test,
-      header: json.header_text.user.error,
-      body1: json.body_text.user.error.l1,
-      body2: json.body_text.user.error.l2,
-      body3: json.body_text.user.error.l3,
-      link: json.link.base + json.link.form,
-      button: json.button_text.user.resubmit
-    }
-    send(params)
+  var formExceptionParams = {
+    to: emailFormParams.req_email,
+    from_name: 'VTrans Rail Division',
+    from_email: json.recipients.mark,
+    reply_to: json.recipients.mark,
+    cc: json.recipients.rpm,
+    bcc: json.recipients.gis + ', ' + json.recipients.rwa,
+    subject: json.subject.user.test,
+    header: json.header_text.user.error,
+    body1: json.body_text.user.error.l1,
+    body2: json.body_text.user.error.l2,
+    body3: json.body_text.user.error.l3,
+    link: json.link.base + json.link.form,
+    button: json.button_text.user.resubmit
   }
-}
 
-function send (params) {
-  console.log(params)
-  console.log('email test sent')
+  var formSuccesUserParams = {
+    to: emailFormParams.req_email,
+    from_name: 'VTrans Rail Division',
+    from_email: json.recipients.mark,
+    reply_to: json.recipients.mark,
+    cc: null,
+    bcc: json.recipients.gis + ', ' + json.recipients.rwa,
+    subject: json.subject.user.test,
+    header: json.header_text.user.success,
+    body1: json.body_text.user.success,
+    body2: null,
+    body3: null,
+    link: json.link.base + json.link.status + json.link.query + emailFormParams.form_number,
+    button: json.button_text.user.resubmit
+  }
 
-  // document.getElementById('emailSuccess').style.display = 'block'
-  // var btns = document.getElementsByClassName('btn')
-  // for (var i = 0; i < btns.length; i++) {
-  //   btns[i].setAttribute('disabled', 'disabled')
-  // }
-  try {
-    emailjs.send('sendgrid', 'email', params)
-    .then(function (response) {
-      console.log('successful email')
-    }, function (err) {
-      console.error('failed - error = ', err)
-      // document.getElementById('emailFail').style.display = 'block'
-    })
-  } catch (e) {
-    console.error(e)
-  } finally {
-    // TODO: show confirmation that the email was sent
+  var formSuccesUserParams = {
+    to: emailFormParams.req_email,
+    from_name: 'VTrans Rail Division',
+    from_email: json.recipients.mark,
+    reply_to: json.recipients.mark,
+    cc: null,
+    bcc: json.recipients.gis + ', ' + json.recipients.rwa,
+    subject: json.subject.user.test,
+    header: json.header_text.user.success,
+    body1: json.body_text.user.success,
+    body2: null,
+    body3: null,
+    link: json.link.base + json.link.status + json.link.query + emailFormParams.form_number,
+    button: json.button_text.user.resubmit
+  }
+
+  var emails = [] // array of emails to send
+
+  if (emailFormParams.source === 'form') {
+    if (emailFormParams.email_type === 'success') {
+      emails.push[formSuccesUserParams, formSuccessRPMParams]
+    } else if (emailFormParams.email_type === 'exception') {
+      emails.push[formExceptionParams]
+    }
+  } else if (emailFormParams.source === 'vtrans') {
+    if (decision === 'approved') {
+
+    } else if (decision === 'rejected') {
+
+    }
+  } else if (emailFormParams.source === 'vrs') {
+    if (decision === 'approved') {
+
+    } else if (decision === 'rejected') {
+
+    }
+  }
+  send(emails)
+
+  function send (params) {
+    console.log(params)
+    console.log('email test sent')
+
+    // document.getElementById('emailSuccess').style.display = 'block'
+    // var btns = document.getElementsByClassName('btn')
+    // for (var i = 0; i < btns.length; i++) {
+    //   btns[i].setAttribute('disabled', 'disabled')
+    // }
+    try {
+      emailjs.send('sendgrid', 'email', params)
+      .then(function (response) {
+        console.log('successful email')
+      }, function (err) {
+        console.error('failed - error = ', err)
+        // document.getElementById('emailFail').style.display = 'block'
+      })
+    } catch (e) {
+      console.error(e)
+    } finally {
+      // TODO: show confirmation that the email was sent
+    }
   }
 }
